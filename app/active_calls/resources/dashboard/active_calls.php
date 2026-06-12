7bdc59429164b67a5041730499d0df281293c6d1
Nam Son <buison.prime@gmail.com>
2026-06-12 23:42:41 +0700

    Doi sang font chu he thong 2

--- app/active_calls/resources/dashboard/active_calls.php
+++ app/active_calls/resources/dashboard/active_calls.php
@@ -80,7 +80,7 @@ echo "<div class='hud_content' ".($widget_details_state == "disabled" ?: "onclic
 	if ($widget_chart_type == 'icon') {
 		echo "<div style='position: relative; display: inline-block;'>\n";
 			echo "<span class='hud_stat'><i class=\"fas " . $widget_icon . " \"></i></span>\n";
-			echo "<span id='calls_active_count' name='calls_active_count' style=\"background-color: " . (!empty($widget_number_background_color) ? $widget_number_background_color : '#22C55E') . "; color: " . (!empty($widget_number_text_color) ? $widget_number_text_color : '#ffffff') . "; font-size: 12px; font-weight: bold; text-align: center; position: absolute; top: 23px; left: 24.5px; padding: 2px 7px 1px 7px; border-radius: 10px; white-space: nowrap;\">0</span>\n";
+			echo "<span id='calls_active_count' name='calls_active_count' style=\"background-color: " . (!empty($widget_number_background_color) ? $widget_number_background_color : '#22C55E') . "; color: " . (!empty($widget_number_text_color) ? $widget_number_text_color : '#ffffff') . "; font-size: 10px; text-align: center; position: absolute; top: 28px; left: 24px; padding: 2px 7px 1px 7px; border-radius: 10px; white-space: nowrap;\">0</span>\n";
 		echo "</div>\n";
 	}
 echo "</div>\n";
--- app/xml_cdr/resources/dashboard/missed_calls.php
+++ app/xml_cdr/resources/dashboard/missed_calls.php
@@ -158,7 +158,7 @@
 	if (!isset($widget_chart_type) || $widget_chart_type == "icon") {
 		echo "	<div style='position: relative; display: inline-block;'>\n";
 		echo "		<span class='hud_stat'><i class=\"fas ".$widget_icon." \"></i></span>\n";
-		echo "		<span style=\"background-color: ".(!empty($widget_number_background_color) ? $widget_number_background_color : '#EF4444')."; color: ".(!empty($widget_number_text_color) ? $widget_number_text_color : '#ffffff')."; font-size: 12px; font-weight: bold; text-align: center; position: absolute; top: 23px; left: 24.5px; padding: 2px 7px 1px 7px; border-radius: 10px; white-space: nowrap;\">".$num_rows."</span>\n";
+		echo "		<span style=\"background-color: ".(!empty($widget_number_background_color) ? $widget_number_background_color : '#EF4444')."; color: ".(!empty($widget_number_text_color) ? $widget_number_text_color : '#ffffff')."; font-size: 10px; text-align: center; position: absolute; top: 28px; left: 24px; padding: 2px 7px 1px 7px; border-radius: 10px; white-space: nowrap;\">".$num_rows."</span>\n";
 		echo "	</div>\n";
 	}
 	echo "</div>\n";
